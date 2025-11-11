eeabe467d49bdeee440599684e3373bac13098aa
WahajK <wahajkhan788@gmail.com>
2025-11-11 20:56:34 +0100

    Made a working base readline loop

--- src/main.c
+++ src/main.c
@@ -6,7 +6,19 @@
 /*   By: muhakhan <muhakhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 17:32:14 by muhakhan          #+#    #+#             */
-/*   Updated: 2025/11/11 17:32:15 by muhakhan         ###   ########.fr       */
+/*   Updated: 2025/11/11 20:55:49 by muhakhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/minishell.h"
+
+int	main(void)
+{
+	while (1)
+	{
+		char *input = readline("minishell$ ");
+		if (!input)
+			exit(0);
+		free(input);
+	}
+}
